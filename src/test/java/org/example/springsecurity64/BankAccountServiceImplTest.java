96e2d002d5f8af7654833097fa24b89884c90a1a
Rob Winch <362503+rwinch@users.noreply.github.com>
2024-08-15 16:53:45 -0500

    @WithMockUser

--- src/test/java/org/example/springsecurity64/BankAccountServiceImplTest.java
+++ src/test/java/org/example/springsecurity64/BankAccountServiceImplTest.java
@@ -2,48 +2,41 @@ package org.example.springsecurity64;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class BankAccountServiceImplTest {
 	BankAccountService account = new BankAccountServiceProxy(new BankAccountServiceImpl());
 
-	@AfterEach
-	void cleanup() {
-		SecurityContextHolder.clearContext();
-	}
-
 	@Test
+	@WithMockUser("rob")
 	void findByIdGranted() {
-		login("rob");
 		this.account.findById(1);
 	}
 
 	@Test
+	@WithMockUser("josh")
 	void findByIdDenied() {
-		login("josh");
 		assertThatExceptionOfType(AuthorizationDeniedException.class)
 				.isThrownBy(() -> this.account.findById(1));
 	}
 
 	@Test
+	@WithMockUser("rob")
 	void getByIdGranted() {
-		login("rob");
 		this.account.getById(1);
 	}
 
 	@Test
+	@WithMockUser("josh")
 	void getByIdDenied() {
-		login("josh");
 		assertThatExceptionOfType(AuthorizationDeniedException.class)
 				.isThrownBy(() -> this.account.getById(1));
-	}
-
-	void login(String username) {
-		SecurityContextHolder.getContext()
-				.setAuthentication(new TestingAuthenticationToken(username, "password", "ROLE_USER"));
 	}
 }
