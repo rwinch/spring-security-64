6493918da2bd938b1b631f4584076e2bb21b72e0
Rob Winch <362503+rwinch@users.noreply.github.com>
2024-08-15 17:07:32 -0500

    @AuthorizeReturnObject

--- src/main/java/org/example/springsecurity64/BankAccountServiceImpl.java
+++ src/main/java/org/example/springsecurity64/BankAccountServiceImpl.java
@@ -1,14 +1,11 @@
 package org.example.springsecurity64;
 
-import org.springframework.security.authorization.AuthorizationProxyFactory;
-import org.springframework.security.authorization.method.AuthorizationAdvisorProxyFactory;
-
 public class BankAccountServiceImpl implements BankAccountService {
 	@Override
 	public BankAccount findById(int id) {
 		BankAccount result = new BankAccount(id, "rob", "1234", 54321);
-		AuthorizationProxyFactory factory = AuthorizationAdvisorProxyFactory.withDefaults();
-		return (BankAccount) factory.proxy(result);
+
+		return result;
 	}
 
 	@Override
--- src/main/java/org/example/springsecurity64/PostReadBankAccount.java
+++ src/main/java/org/example/springsecurity64/PostReadBankAccount.java
@@ -1,11 +1,13 @@
 package org.example.springsecurity64;
 
 import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.authorization.method.AuthorizeReturnObject;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 @PostAuthorize("returnObject?.owner == authentication?.name or hasRole('ACCOUNTANT')")
+@AuthorizeReturnObject
 public @interface PostReadBankAccount {
 }
